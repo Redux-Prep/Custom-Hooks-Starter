36e5aa491d9e4921821a19feff5a1660170a659d
Justin Trombley <jtrombley96@gmail.com>
2020-01-15 12:12:09 -0500

    Boilerplate

---
+++ custom-hooks/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SignupForm from './components/SignupForm.js';
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Custom Hooks are Magic 🧙‍♂️</h1>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App
